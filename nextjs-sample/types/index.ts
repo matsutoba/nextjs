f9f558b8a4fd9f8d175221f9a248b3b037ba6f89
Toshiyuki Matsubara <matsubara_t@outlook.jp>
2023-11-24 18:57:27 +0900

    notionからデータを取得して表示

---
+++ nextjs-sample/types/index.ts
@@ -0,0 +1,13 @@
+export type Content = {
+  type: 'paragraph' | 'quote' | 'heading_2' | 'heading_3';
+  text: string | null;
+}
+
+export type Post = {
+  id: string;
+  title: string | null;
+  slug: string | null;
+  createdTs: string | null;
+  lastEditedTs: string | null;
+  contents: Content[];
+}
